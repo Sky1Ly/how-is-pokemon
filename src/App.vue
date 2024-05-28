b8fc707f7da0048f3fe460e829ca52b6f4ddaaf3
Eider Agudelo <eider841agudelo@hotmail.com>
2024-05-28 12:18:30 -0400

    Se hace todas las funcionalidades en App.vue

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,8 @@ export default {
     return {
       getDataApi: [],
       dataPokemon: {},
-      nombrePokemon: {}
+      nombrePokemon: [],
+      count: 0
     }
   },
 
@@ -22,19 +23,32 @@ export default {
         const promesa = this.getDataApi.results.map(async (pokemon) => {
           const { data } = await axios.get(pokemon.url)
 
+          //URL para saber nombres de pokemons en consola
+        console.log(`Haz descubierto los nombres :o \n ${pokemon.name}`);
+
           return {
             name: pokemon.name,
             image: data.sprites?.front_default,
-            gif: data.sprites?.other?.showdown?.front_default
+            gif: data.sprites?.other?.showdown?.front_default,
+            porDescurbri: true
           }
         })
 
         this.dataPokemon = await Promise.all(promesa)
 
-        console.log(this.dataPokemon);
+        // Inicializar el array de nombres
+        this.nombrePokemon = new Array(this.dataPokemon.length).fill("")
 
       } catch (error) {
         console.error(`Ha ocurrido un error: ${error}`);
+      }
+    },
+
+    descubrirPokemon(index) {
+      if (this.nombrePokemon[index].toLowerCase() == this.dataPokemon[index].name.toLowerCase()) {
+        // Actualizar la propiedad descubierto
+        this.dataPokemon[index].porDescurbri = false
+        this.count++
       }
     }
   },
@@ -51,17 +65,18 @@ export default {
   <div class="container">
     <div class="text-center mt-5 mb-5">
       <img src="./assets/pokemon-logo.png" alt="Pokemon logo" style="max-width: 350px;" />
-      <h1>How is Pokemon?</h1>
+      <h1>What is Pokemon?</h1>
+      <h3>discovered Pokémon: {{ count }}</h3>
     </div>
 
     <div class="row">
       <div v-for="(data, i) in dataPokemon" :key="i" class="col-sm-3 mx-auto mb-5">
-        <img :src="data.image" style="filter: blur(2px) grayscale(100%);" />
-        <br/>
-        <input type="text" name="inputNombre" v-model="nombrePokemon">
-        <p>{{ data.name }}</p>
-        <button type="button" class="btn btn-outline-success">Descubrir</button>        
-        <p>{{ nombrePokemon }}</p>
+        <img v-if="data.porDescurbri" :src="data.image" :class="{descubrir: data.porDescurbri}" />
+        <img v-if="!data.porDescurbri" :src="data.gif" />
+        <br />
+        <input type="text" name="inputNombre" v-model="nombrePokemon[i]" v-if="data.porDescurbri">
+        <p v-if="!data.porDescurbri">{{ data.name }}</p>
+        <button v-if="data.porDescurbri" type="button" class="btn btn-outline-success mt-3" @click="descubrirPokemon(i)">Descubrir</button>
       </div>
     </div>
   </div>
@@ -72,5 +87,9 @@ h1 {
   font-family: "Press Start 2P", system-ui;
   font-weight: 400;
   font-style: normal;
+}
+
+.descubrir {
+  filter: blur(2px) grayscale(100%);
 }
 </style>
